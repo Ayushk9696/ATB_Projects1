9d36223d2963a6f7f31543b0088bbb1efa0a29a9
Ayush <ayush.kadam44@gmail.com>
2024-09-23 17:47:44 +0530

    new

---
+++ src/Sept/ex_04092024/ex_04092024.java
@@ -0,0 +1,11 @@
+package Sept.ex_04092024;
+
+public class ex_04092024 {
+
+    public static void main(String[] args) {
+        int a=2;
+        char c ='A';
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
---
+++ src/ex_16092024/Lab003.java
@@ -0,0 +1,16 @@
+package ex_16092024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        byte a=5;
+        byte b=10;
+        boolean c =a>b;
+
+        System.out.println(c);
+        System.out.println(a-b);
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println("+"+b);
+    }
+}
